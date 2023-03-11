e92a0dfa3a69edf8c91902699afba8fb8c628d0d
ytikhov <ahojte@gmail.com>
2023-03-11 23:42:38 +0300

    jpa optimistic lock

--- jpa-locks/src/main/kotlin/com/ytikhov/jpalocks/repo/SharedRepository.kt
+++ jpa-locks/src/main/kotlin/com/ytikhov/jpalocks/repo/SharedRepository.kt
@@ -2,11 +2,5 @@ package com.ytikhov.jpalocks.repo
 
 import com.ytikhov.jpalocks.model.SharedEntity
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Lock
-import java.util.*
-import javax.persistence.LockModeType
 
-interface SharedRepository: JpaRepository<SharedEntity, Long> {
-    @Lock(LockModeType.OPTIMISTIC)
-    override fun findById(id: Long): Optional<SharedEntity>
-}
+interface SharedRepository: JpaRepository<SharedEntity, Long>
